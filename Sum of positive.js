3f26eb35c4a1c97e06e16b074ddff26eeecf1464
romainpro <romain.casagrande.pro@gmail.com>
2023-06-07 15:08:27 +0200

    Sum of positive

---
+++ Sum of positive.js
@@ -0,0 +1,14 @@
+function positiveSum(arr) {
+    const arrayClean = []
+   for (let i = 0 ;i <arr.length;i++){
+     if(arr.length == 0){
+       return 0
+     }
+     else if(arr[i]>=0){
+        arrayClean.push(arr[i])
+       }
+   }
+    console.log(arrayClean)
+    let arrayTotal = arrayClean.reduce((a,b)=>a+b ,0);
+    return arrayTotal
+  }
